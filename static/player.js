0ecd9743b9bed63ea6a08c4e5612b7631b22dabc
Michael O'Keefe <mikeco57@gmail.com>
2019-02-05 09:10:54 -0500

    Integrate videojs-chromecast with player1.html

--- static/player.js
+++ static/player.js
@@ -1,6 +1,18 @@
+var options;
+
+options = {
+   controls: true,
+   techOrder: [ 'chromecast', 'html5' ], // You may have more Tech, such as Flash or HLS
+   plugins: {
+      chromecast: {
+        preloadWebComponents: true
+      }
+   },
+};
+
 // Generate a "unique" ID
 var id = new Date().getTime();
-var player = videojs("player");
+var player = videojs("player", options);
 
 function pingServer() {
     $.post('/api/player', {
